6303c91c3a4a8ce3afc451399ede1fc55d414b37
Rishabh Agrawal <rishabh120296@gmail.com>
2018-07-09 16:32:37 +0530

    Fixed linting issues

--- reverse-using-iterators.js
+++ reverse-using-iterators.js
@@ -1,6 +1,6 @@
 // The intent of this file is to reverse the string using iterators (for, foreach)
 const reverse = (inputString) => {
-  const length = inputString.length;
+  const { length } = inputString;
   let reverseString = '';
   let i = 0;
   for (i = 0; i < length; i += 1) {
